e441933c176d692cdd29f28d9e37860189ffa831
Lars Gierth <lars.gierth@gmail.com>
2013-09-14 01:31:02 +0200

    Implement 3.2.1

--- lib/promise.rb
+++ lib/promise.rb
@@ -1,12 +1,16 @@
 require 'promise/version'
 
 class Promise
-  def then(on_fulfill, on_reject)
+  def then(on_fulfill = nil, on_reject = nil)
     @on_fulfill = on_fulfill
     @on_reject = on_reject
   end
 
+  def fulfill(value)
+    @on_fulfill.call(value) if @on_fulfill
+  end
+
   def reject(reason)
-    @on_reject.call(reason)
+    @on_reject.call(reason) if @on_reject
   end
 end
--- spec/promise_spec.rb
+++ spec/promise_spec.rb
@@ -6,13 +6,22 @@ describe Promise, '#then' do
   let(:value) { double('value') }
   let(:reason) { double('reason') }
 
+  let(:on_fulfill) do
+    proc { |value| @value = value }
+  end
+  let(:on_reject) do
+    proc { |reason| @reason = reason }
+  end
+
   describe '3.2.1' do
     specify 'on_fulfill is optional' do
-      result = nil
-      subject.then(nil, proc {|r| result = r })
+      subject.then
+      subject.fulfill(value)
+    end
 
+    specify 'on_reject is optional' do
+      subject.then(on_fulfill)
       subject.reject(reason)
-      expect(result).to eq(reason)
     end
   end
 end
