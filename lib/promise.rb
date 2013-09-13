2aa9f764e6ee79e4d45ab1cb405fcd550a9a2f6a
Lars Gierth <lars.gierth@gmail.com>
2013-09-14 01:47:05 +0200

    Specify 3.2.2

--- lib/promise.rb
+++ lib/promise.rb
@@ -1,13 +1,22 @@
 require 'promise/version'
 
 class Promise
+  attr_reader :state
+
+  def initialize
+    @state = :pending
+  end
+
   def then(on_fulfill = nil, on_reject = nil)
     @on_fulfill = on_fulfill
     @on_reject = on_reject
   end
 
   def fulfill(value)
-    @on_fulfill.call(value) if @on_fulfill
+    if @state == :pending
+      @state = :fulfilled
+      @on_fulfill.call(value) if @on_fulfill
+    end
   end
 
   def reject(reason)
--- spec/promise_spec.rb
+++ spec/promise_spec.rb
@@ -6,13 +6,6 @@ describe Promise, '#then' do
   let(:value) { double('value') }
   let(:reason) { double('reason') }
 
-  let(:on_fulfill) do
-    proc { |value| @value = value }
-  end
-  let(:on_reject) do
-    proc { |reason| @reason = reason }
-  end
-
   describe '3.2.1' do
     specify 'on_fulfill is optional' do
       subject.then
@@ -20,8 +13,43 @@ describe Promise, '#then' do
     end
 
     specify 'on_reject is optional' do
-      subject.then(on_fulfill)
+      subject.then(proc { |_| })
       subject.reject(reason)
+    end
+  end
+
+  describe '3.2.2' do
+    specify 'on_fulfill is called after promise is fulfilled' do
+      state = nil
+      subject.then(proc { |_| state = subject.state })
+
+      subject.fulfill(value)
+      expect(state).to eq(:fulfilled)
+    end
+
+    specify 'on_fulfill is called with fulfillment value' do
+      result = nil
+      subject.then(proc { |val| result = val })
+
+      subject.fulfill(value)
+      expect(result).to eq(value)
+    end
+
+    specify 'on_fulfill is called not more than once' do
+      called = 0
+      subject.then(proc { |_| called += 1 })
+
+      subject.fulfill(value)
+      subject.fulfill(value)
+      expect(called).to eq(1)
+    end
+
+    specify 'on_fulfill is not called if on_reject has been called' do
+      called = false
+      subject.then(proc { |_| called = true })
+
+      subject.reject(reason)
+      expect(called).to eq(false)
     end
   end
 end
