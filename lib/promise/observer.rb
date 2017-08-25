e5b65212b8bf62d57e44309da10557a86ebf667c
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2017-08-25 13:00:30 -0400

    Fix method reference in documentation for Promise::Observer

--- lib/promise/observer.rb
+++ lib/promise/observer.rb
@@ -2,7 +2,7 @@ class Promise
   # The `Promise::Observer` module allows an object to be
   # notified of `Promise` state changes.
   #
-  # See `Promise#observe`.
+  # See `Promise#subscribe`.
   module Observer
     def promise_fulfilled(_value, _on_fulfill_arg); end
 
